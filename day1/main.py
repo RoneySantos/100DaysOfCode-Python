1015b1bd5d3cb3b0a0a6879821112a1790dcad7d
Roney Santos <joseroneysilvasantos@gmail.com>
2021-10-27 17:59:15 -0300

    primeira parada da aula 1

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,4 @@
+##### LESSON 1 ######
 # print("Ola Mundo")
 
 # print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what  print')")
@@ -5,9 +6,22 @@
 # #print("print('what  print')")
 # print ("Hello" + " " + "Angela")
 
+##### LESSON 2 ######
 #Fix the code below 👇
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+##### LESSON 3 ######
+
+# print("Hello " + input("Whats your name") + "!")
+
+##### LESSON 4 ######
+
+print( 
+    len( 
+    input("Escreva seu nome aqui !") 
+    ) 
+)
