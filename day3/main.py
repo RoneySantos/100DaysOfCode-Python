5951bb4c4b5c76fe3579e1aa0c1f0c601fbe3a8d
System Administrator <root@MacBook-Air-de-Brenda.local>
2021-11-03 22:34:32 -0300

    desafio do par ou impar

--- day3/main.py
+++ day3/main.py
@@ -1,7 +1,17 @@
-##teste
-water_level = 90
+# ##teste
+# water_level = 50
 
-if water_level > 80:
- print("para a agua")
+# if water_level > 80:
+#  print("para a agua")
+# else:
+#  print("Continua")
+
+calcula_numero = int(input("Input HERE some number, to be calculate if is odd or an ! \n")) % 2
+#calcula_numero = numero % 2
+
+if calcula_numero == 0:
+    print("Number is an")
 else:
- print("Continua")
+    print("Number is odd")
+
+print ("Finishe the challenge !")
